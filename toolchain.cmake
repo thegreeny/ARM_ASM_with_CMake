348114cad240dfac66d8caf8b7ba25df357b3521
Sven Reis <sven@dr-greeny.de>
2022-11-23 19:08:42 +0100

    Moved compile_options to toolchain-file

--- toolchain.cmake
+++ toolchain.cmake
@@ -7,6 +7,10 @@ set(CMAKE_CXX_COMPILER  "arm-none-eabi-g++"    )
 set(CMAKE_ASM_COMPILER  "arm-none-eabi-gcc"    )
 set(OBJ_COPY_PATH       "arm-none-eabi-objcopy")
 
+# Set the Flags for Cortex-M0
+set(CMAKE_C_FLAGS       "-mcpu=cortex-m0")
+set(CMAKE_ASM_FLAGS     "-mcpu=cortex-m0")
+
 # With startup files (auto-init):
 #set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs" CACHE INTERNAL "")
 
