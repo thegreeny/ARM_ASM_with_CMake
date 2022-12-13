a5aa499d2c2e1114eb6c638466230dd566ce16e0
Thomas Vogt <thomas@thomas-vogt.de>
2022-12-13 14:22:15 +0100

    simplified Hello World

--- HelloWorld.s
+++ HelloWorld.s
@@ -3,7 +3,6 @@
 
 # Hier gehts los
 _start:
-    mov r0,#0x100
     adr r0,thumb_start
     add r0,r0,#1
     bx r0
@@ -15,5 +14,6 @@ thumb_start:
 #    mov r0,#0x100
     MOVS r0, #1      @ Lade "1"
     MOVS r1, #2      @ Lade "2"
-    ADD r2, r0, r1  @ Addiere "1" + "2"
-    B thumb_start
+    ADD r2, r0, r1   @ Addiere "1" + "2"
+endless_start:
+    B endless_start
